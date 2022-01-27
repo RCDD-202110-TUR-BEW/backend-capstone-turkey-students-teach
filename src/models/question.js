14919d6c8bded661b4d3318a894c245aa3cf86d7
Muhammeday99 <mohammed.ay99@gmail.com>
2022-01-28 01:25:58 +0300

    created student and question models

---
+++ src/models/question.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+
+const subject = mongoose.Schema({
+  title: {
+    type: String,
+    enum: ['Math', 'Physics', 'Biology', 'History', 'Programming'],
+    default: 'Math',
+    required: true
+  }
+})
+
+const question = mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  subjects: {
+    type: [subject],
+  },
+  isSolved: {
+    type: Boolean,
+    default: false
+  },
+  // comments: {
+  //   type: [comment]
+  // },
+  student: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Student',
+    required: true
+  }
+})
+
+
+module.exports = mongoose.model('Question', question)
---
+++ src/models/student.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose')
+
+const subject = mongoose.Schema({
+  title: {
+    type: String,
+    enum: ['Math', 'Physics', 'Biology', 'History', 'Programming'],
+    default: 'Math',
+    required: true
+  }
+})
+
+const student = mongoose.schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  password_hash: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  isTutor: {
+    type: Boolean,
+    default: false
+  },
+  avatar: {
+    type: String
+  },
+  subjects: {
+    type: [subject],
+  },
+  questions: {
+    type: [mongoose.Schema.Types.ObjectId],
+    ref: 'Question'
+  },
+  inbox: {
+    type: [mongoose.Schema.Types.ObjectId],
+    ref: 'messagingChannel'
+  }
+})
+
+module.exports = mongoose.model('Student', student)
