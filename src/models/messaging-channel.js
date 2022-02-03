8b63ed5af3efee50fa42079a0b0c2e48a6ad3ace
inas alarabi <inasalarabi@gmail.com>
2022-02-03 21:12:08 +0300

    local changes

--- src/models/messaging-channel.js
+++ src/models/messaging-channel.js
@@ -1,37 +1,38 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+// const mongoose = require('mongoose');
 
-const messageSchema = new Schema(
-  {
-    content: {
-      type: String,
-      required: true,
-    },
-    sender: {
-      type: Schema.Types.ObjectId,
-      ref: "Student",
-      required: true,
-    },
-    isRead: {
-      type: Boolean,
-      required: true,
-      default: false,
-    },
-  },
-  { timestamps: true }
-);
-const messagingChannnelSchema = new Schema(
-    {
-      contacts: {
-        type: [Schema.Types.ObjectId],
-        ref: "Student",
-        required: true,
-      },
-      messages: {
-        type: [messageSchema],
-        default: [],
-      },
-    },
-    { timestamps: true }
-  );
-module.exports = mongoose.model("MessagingChannel", messagingChannnelSchema);
+// const Schema = mongoose.Schema;
+
+// const messageSchema = new Schema(
+//   {
+//     content: {
+//       type: String,
+//       required: true,
+//     },
+//     sender: {
+//       type: Schema.Types.ObjectId,
+//       ref: 'Student',
+//       required: true,
+//     },
+//     isRead: {
+//       type: Boolean,
+//       required: true,
+//       default: false,
+//     },
+//   },
+//   { timestamps: true }
+// );
+// const messagingChannnelSchema = new Schema(
+//   {
+//     contacts: {
+//       type: [Schema.Types.ObjectId],
+//       ref: 'Student',
+//       required: true,
+//     },
+//     messages: {
+//       type: [messageSchema],
+//       default: [],
+//     },
+//   },
+//   { timestamps: true }
+// );
+// module.exports = mongoose.model('MessagingChannel', messagingChannnelSchema);
--- src/models/question.js
+++ src/models/question.js
@@ -1,38 +1,39 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+// const mongoose = require('mongoose');
 
-const commentSchema = new Schema(
-  {
-    content: {
-      type: String,
-      required: true,
-    },
-    question: {
-      type: Schema.Types.ObjectId,
-      ref: "Question",
-      required: true,
-    },
-    creator: {
-        type: Schema.Types.ObjectId,
-        ref: "Student",
-        required: true,
-      },
-    isRead: {
-      type: Boolean,
-      required: true,
-      default: false,
-    },
-  },
-  { timestamps: true }
-);
-const questionSchema = new Schema(
-    {
-        //  TODO: add question properties
-      comments: {
-        type: [commentSchema],
-        default: [],
-      },
-    },
-    { timestamps: true }
-  );
-module.exports = mongoose.model("question", questionSchema);
+// const Schema = mongoose.Schema;
+
+// const commentSchema = new Schema(
+//   {
+//     content: {
+//       type: String,
+//       required: true,
+//     },
+//     question: {
+//       type: Schema.Types.ObjectId,
+//       ref: 'Question',
+//       required: true,
+//     },
+//     creator: {
+//       type: Schema.Types.ObjectId,
+//       ref: 'Student',
+//       required: true,
+//     },
+//     isRead: {
+//       type: Boolean,
+//       required: true,
+//       default: false,
+//     },
+//   },
+//   { timestamps: true }
+// );
+// const questionSchema = new Schema(
+//   {
+//     //  TODO: add question properties
+//     comments: {
+//       type: [commentSchema],
+//       default: [],
+//     },
+//   },
+//   { timestamps: true }
+// );
+// module.exports = mongoose.model('question', questionSchema);
