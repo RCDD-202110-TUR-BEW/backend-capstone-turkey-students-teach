c229ebe18c71995b4bb3078aee44415f0ec8bb3e
emirsagit <emirsagit@gmail.com>
2022-02-07 21:40:27 +0300

    add signup functionality

--- src/app.js
+++ src/app.js
@@ -1,16 +1,20 @@
+require('dotenv').config();
 const express = require('express');
-
 const router = require('./routes');
 const tutorRouter = require('./routes/tutor');
 const questionRouter = require('./routes/question');
+const authRouter = require('./routes/auth');
+const connectToMongo = require('./db/connection');
+
+const app = express();
+const port = process.env.NODE_LOCAL_PORT;
+
+app.use(express.json());
+
 app.use('/', router);
 app.use('/tutors', tutorRouter);
 app.use('/questions', questionRouter);
-
-require('dotenv').config();
-const connectToMongo = require('./db/connection');
-const app = express();
-const port = process.env.NODE_LOCAL_PORT;
+app.use('/auth', authRouter);
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
@@ -19,4 +23,3 @@ app.listen(port, () => {
 });
 
 module.exports = app;
-
--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,5 +1,90 @@
+// username: {
+//   type: String,
+//   required: true,
+//   unique: true,
+// },
+// firstName: {
+//   type: String,
+//   required: true,
+// },
+// lastName: {
+//   type: String,
+//   required: true,
+// },
+// passwordHash: {
+//   type: String,
+//   required: true,
+// },
+// email: {
+//   type: String,
+//   required: true,
+// },
+// isTutor: {
+//   type: Boolean,
+//   default: false,
+// },
+// subjects: {
+//   type: [subject],
+// },
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { studentModel } = require('../models/student');
+
+function generateAccessToken(student) {
+  return jwt.sign(student, process.env.TOKEN_SECRET, { expiresIn: '1800s' });
+}
+
 module.exports = {
-  signin: async () => {},
+  signin: async (req, res) => {},
+  signup: async (req, res) => {
+    const {
+      username,
+      firstName,
+      lastName,
+      password,
+      avatar,
+      email,
+      isTutor,
+      subjects,
+    } = req.body;
+
+    if (!password || password.length < 6) {
+      return res
+        .status(400)
+        .json({ error: 'password should longer than 6 characters' });
+    }
+
+    // Check username is unique
+    try {
+      let student = await studentModel.findOne({ username });
+
+      if (student) {
+        return res
+          .status(400)
+          .json({ error: `${username}: username already used` });
+      }
+
+      const passwordHash = await bcrypt.hash(password, 10);
+
+      student = studentModel.create({
+        firstName,
+        lastName,
+        username,
+        avatar,
+        passwordHash,
+        email,
+        isTutor,
+        'subjects.title': subjects,
+      });
+
+      const token = generateAccessToken({ student });
+
+      console.log(`token,${token}`);
+
+      return res.status(200).json(token);
+    } catch (error) {
+      return res.status(400).json(error);
+    }
+  },
   signInWithGmail: async () => {},
-  signup: async () => {},
 };
