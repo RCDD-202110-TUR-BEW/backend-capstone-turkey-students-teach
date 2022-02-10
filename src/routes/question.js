a85e5f3db48080715024b48c1f0426dbe62583fa
Muhammeday99 <mohammed.ay99@gmail.com>
2022-02-11 02:02:21 +0300

    added tests for endpoints

--- src/routes/question.js
+++ src/routes/question.js
@@ -4,36 +4,36 @@ const questionController = require('../controllers/question');
 
 const router = express.Router();
 
-// router.get('/', questionController.getAllQuestions);
+router.get('/', questionController.getAllQuestions);
 
-// router.post('/', onlyAuthenticated(), questionController.addNewQuestion);
+router.post('/', onlyAuthenticated(), questionController.addNewQuestion);
 
-// router.get('/search', questionController.searchForQuestions);
+router.get('/search', questionController.searchForQuestions);
 
-// router.get('/filter/:tagid', questionController.getQuestiosWithSimilarTags);
+router.get('/filter/:tagid', questionController.getQuestiosWithSimilarTags);
 
-// router.get('/:id', questionController.getOneQuestion);
+router.get('/:id', questionController.getOneQuestion);
 
-// router.put('/:id', onlyAuthenticated(), questionController.updateQuestion);
+router.put('/:id', onlyAuthenticated(), questionController.updateQuestion);
 
-// router.delete('/:id', onlyAuthenticated(), questionController.deleteQuestion);
+router.delete('/:id', onlyAuthenticated(), questionController.deleteQuestion);
 
-// router.post(
-//   '/:id/comments',
-//   onlyAuthenticated(),
-//   questionController.addComment
-// );
+router.post(
+  '/:id/comments',
+  onlyAuthenticated(),
+  questionController.addComment
+);
 
-// router.put(
-//   ':id/comments/:commentid',
-//   onlyAuthenticated(),
-//   questionController.updateComment
-// );
+router.put(
+  ':id/comments/:commentid',
+  onlyAuthenticated(),
+  questionController.updateComment
+);
 
-// router.delete(
-//   ':id/comments/:commentid',
-//   onlyAuthenticated(),
-//   questionController.deleteComment
-// );
+router.delete(
+  ':id/comments/:commentid',
+  onlyAuthenticated(),
+  questionController.deleteComment
+);
 
 module.exports = router;
