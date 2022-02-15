e5f7c2bfa13d4a5306c08b92de5f1a2b31d050f1
Abdulrahman Alfawal <abdulrahman.hadi4@gmail.com>
2022-02-15 18:46:07 +0300

    add tutor controller functionality

--- src/app.js
+++ src/app.js
@@ -1,16 +1,17 @@
+require('dotenv').config();
 const express = require('express');
+const connectToMongo = require('./db/connection');
 
 const router = require('./routes');
 const tutorRouter = require('./routes/tutor');
 const questionRouter = require('./routes/question');
+
+const app = express();
+const port = process.env.NODE_LOCAL_PORT;
+
 app.use('/', router);
 app.use('/tutors', tutorRouter);
 app.use('/questions', questionRouter);
-
-require('dotenv').config();
-const connectToMongo = require('./db/connection');
-const app = express();
-const port = process.env.NODE_LOCAL_PORT;
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
@@ -19,4 +20,3 @@ app.listen(port, () => {
 });
 
 module.exports = app;
-
