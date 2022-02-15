65ef83683fdc6e48067cb8cf0ba41ff4f73aadfb
Abdulrahman Alfawal <abdulrahman.hadi4@gmail.com>
2022-02-15 18:54:56 +0300

    add onlyAuthenticated by ihab

---
+++ src/middleware/onlyAuthenticated.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+
+//  error messages
+const sessionNotFoundError = {
+  meesage: 'you are not signed in. Please sign in',
+};
+
+// eslint-disable-next-line consistent-return
+module.exports = function onlyAuthenticated(req, res, next) {
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token == null) return res.status(401).json(sessionNotFoundError);
+  // eslint-disable-next-line consistent-return
+  jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+    if (err) return res.sendStatus(403);
+
+    req.user = user;
+
+    next();
+  });
+};
