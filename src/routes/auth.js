fadb0f62210e673123a319c98222c3f2529480ec
Emir Sağıt <emirsagit@gmail.com>
2022-02-24 15:15:57 +0300

    Feature:add validator to auth (#31)
    * Feature:add validator to auth
    
    * Style:fix typo
    
    Co-authored-by: Abdulrahman Alfawal <61620817+alfawal@users.noreply.github.com>

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const authController = require('../controllers/auth');
+const {
+  validateSignin,
+  validateSignup,
+} = require('../controllers/validators/authValidator');
 
 const router = express.Router();
 
-router.post('/signin', authController.signin);
+router.post('/signin', validateSignin, authController.signin);
 
 router.post('/signin/google', authController.signInWithGmail);
 
-router.post('/signup', authController.signup);
+router.post('/signup', [validateSignin, validateSignup], authController.signup);
 
 router.get('/signin', (req, res) => {
   res.render('login');
