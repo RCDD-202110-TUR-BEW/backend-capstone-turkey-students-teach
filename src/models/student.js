76921d8daf53551bdab9668788bf0501bb0fa94e
inas alarabi <inasalarabi@gmail.com>
2022-02-25 22:45:54 +0300

    fix tutor model issue

--- src/models/student.js
+++ src/models/student.js
@@ -55,25 +55,17 @@ const student = mongoose.Schema({
     type: [mongoose.Schema.Types.ObjectId],
     ref: 'MessagingChannel',
   },
-},
-{
-  toObject: {
-    virtuals: true,
-  },
-  toJSON: {
-    virtuals: true,
-  },
 });
 
 student
-.virtual('fullName')
-.get(function () {
-  return `${this.firstName} ${this.lastName}`;
-})
-.set(function (fullName) {
-  this.set('firstName', fullName.substr(0, fullName.indexOf(' ')));
-  this.set('lastName', fullName.substr(fullName.indexOf(' ') + 1));
-});
+  .virtual('fullName')
+  .get(function () {
+    return `${this.firstName} ${this.lastName}`;
+  })
+  .set(function (fullName) {
+    this.set('firstName', fullName.substr(0, fullName.indexOf(' ')));
+    this.set('lastName', fullName.substr(fullName.indexOf(' ') + 1));
+  });
 
 // Prevent password to send
 student.set('toJSON', {
@@ -83,6 +75,8 @@ student.set('toJSON', {
     return ret;
   },
 });
+student.set('toObject', { virtuals: true });
+student.set('toJSON', { virtuals: true });
 
 module.exports = {
   StudentModel: mongoose.model('Student', student),
