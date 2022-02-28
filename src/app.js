2e8fc72eb1047cfad05ba9a6a7903f904182e9d9
Emir Sağıt <emirsagit@gmail.com>
2022-03-01 00:01:48 +0300

    Feature:add cors and getStudent method (#38)
    * Feature:add cors and getStudent method
    
    * Add google client id
    
    * Fix:question controller problem
    
    * Fix:question controller logic problem

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 const router = require('./routes');
@@ -15,12 +16,15 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+  })
+);
 
 const port = process.env.NODE_LOCAL_PORT;
 
 app.set('view engine', 'ejs');
-
-app.use(express.json());
 
 app.use('/', router);
 app.use('/tutors', tutorRouter);
--- src/middleware/getAuthUser.js
+++ src/middleware/getAuthUser.js
@@ -1,20 +1,14 @@
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
-//  error messages
-const sessionNotFoundError = {
-  meesage: 'you are not signed in. Please sign in',
-};
-
 // eslint-disable-next-line consistent-return
 module.exports = function onlyAuthenticated(req, res, next) {
   const authHeader = req.headers.authorization;
   const token = authHeader && authHeader.split(' ')[1];
-  if (token == null) return res.status(401).json(sessionNotFoundError);
+  if (token == null) return next();
   // eslint-disable-next-line consistent-return
   jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403);
-
+    if (err) return next();
     req.user = user.student;
 
     next();
